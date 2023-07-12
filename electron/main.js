904c4762cefb025a935c177571ef08da99153338
Grelm <139367581+gre1m@users.noreply.github.com>
2023-07-12 18:04:50 +0300

    Update main.js

--- electron/main.js
+++ electron/main.js
@@ -186,7 +186,7 @@ if (process.platform !== 'darwin') {
     });
   }
 }
-// This method will be called when Electron has finished
+// This method will be called when Electron has finished work
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', () => {
